2b3ecb09f59c8f181444a73abae0b4facce215a5
Alexander Perov <alexander.perov@gmail.com>
2014-11-03 00:08:59 +0400

    update

--- src/Browser/Console.php
+++ src/Browser/Console.php
@@ -3,114 +3,10 @@
 namespace Sb\Browser;
 
 use Sunra\PhpSimple\HtmlDomParser;
-
-if (!function_exists('http_build_url'))
-{
-    define('HTTP_URL_REPLACE', 1);              // Replace every part of the first URL when there's one of the second URL
-    define('HTTP_URL_JOIN_PATH', 2);            // Join relative paths
-    define('HTTP_URL_JOIN_QUERY', 4);           // Join query strings
-    define('HTTP_URL_STRIP_USER', 8);           // Strip any user authentication information
-    define('HTTP_URL_STRIP_PASS', 16);          // Strip any password authentication information
-    define('HTTP_URL_STRIP_AUTH', 32);          // Strip any authentication information
-    define('HTTP_URL_STRIP_PORT', 64);          // Strip explicit port numbers
-    define('HTTP_URL_STRIP_PATH', 128);         // Strip complete path
-    define('HTTP_URL_STRIP_QUERY', 256);        // Strip query string
-    define('HTTP_URL_STRIP_FRAGMENT', 512);     // Strip any fragments (#identifier)
-    define('HTTP_URL_STRIP_ALL', 1024);         // Strip anything but scheme and host
-
-    // Build an URL
-    // The parts of the second URL will be merged into the first according to the flags argument.
-    //
-    // @param   mixed           (Part(s) of) an URL in form of a string or associative array like parse_url() returns
-    // @param   mixed           Same as the first argument
-    // @param   int             A bitmask of binary or'ed HTTP_URL constants (Optional)HTTP_URL_REPLACE is the default
-    // @param   array           If set, it will be filled with the parts of the composed url like parse_url() would return
-    function http_build_url($url, $parts=array(), $flags=HTTP_URL_REPLACE, &$new_url=false)
-    {
-        $keys = array('user','pass','port','path','query','fragment');
-
-        // HTTP_URL_STRIP_ALL becomes all the HTTP_URL_STRIP_Xs
-        if ($flags & HTTP_URL_STRIP_ALL)
-        {
-            $flags |= HTTP_URL_STRIP_USER;
-            $flags |= HTTP_URL_STRIP_PASS;
-            $flags |= HTTP_URL_STRIP_PORT;
-            $flags |= HTTP_URL_STRIP_PATH;
-            $flags |= HTTP_URL_STRIP_QUERY;
-            $flags |= HTTP_URL_STRIP_FRAGMENT;
-        }
-        // HTTP_URL_STRIP_AUTH becomes HTTP_URL_STRIP_USER and HTTP_URL_STRIP_PASS
-        else if ($flags & HTTP_URL_STRIP_AUTH)
-        {
-            $flags |= HTTP_URL_STRIP_USER;
-            $flags |= HTTP_URL_STRIP_PASS;
-        }
-
-        // Parse the original URL
-        $parse_url = parse_url($url);
-
-        // Scheme and Host are always replaced
-        if (isset($parts['scheme']))
-            $parse_url['scheme'] = $parts['scheme'];
-        if (isset($parts['host']))
-            $parse_url['host'] = $parts['host'];
-
-        // (If applicable) Replace the original URL with it's new parts
-        if ($flags & HTTP_URL_REPLACE)
-        {
-            foreach ($keys as $key)
-            {
-                if (isset($parts[$key]))
-                    $parse_url[$key] = $parts[$key];
-            }
-        }
-        else
-        {
-            // Join the original URL path with the new path
-            if (isset($parts['path']) && ($flags & HTTP_URL_JOIN_PATH))
-            {
-                if (isset($parse_url['path']))
-                    $parse_url['path'] = rtrim(str_replace(basename($parse_url['path']), '', $parse_url['path']), '/') . '/' . ltrim($parts['path'], '/');
-                else
-                    $parse_url['path'] = $parts['path'];
-            }
-
-            // Join the original query string with the new query string
-            if (isset($parts['query']) && ($flags & HTTP_URL_JOIN_QUERY))
-            {
-                if (isset($parse_url['query']))
-                    $parse_url['query'] .= '&' . $parts['query'];
-                else
-                    $parse_url['query'] = $parts['query'];
-            }
-        }
-
-        // Strips all the applicable sections of the URL
-        // Note: Scheme and Host are never stripped
-        foreach ($keys as $key)
-        {
-            if ($flags & (int)constant('HTTP_URL_STRIP_' . strtoupper($key)))
-                unset($parse_url[$key]);
-        }
-
-
-        $new_url = $parse_url;
-
-        return
-            ((isset($parse_url['scheme'])) ? $parse_url['scheme'] . '://' : '')
-            .((isset($parse_url['user'])) ? $parse_url['user'] . ((isset($parse_url['pass'])) ? ':' . $parse_url['pass'] : '') .'@' : '')
-            .((isset($parse_url['host'])) ? $parse_url['host'] : '')
-            .((isset($parse_url['port'])) ? ':' . $parse_url['port'] : '')
-            .((isset($parse_url['path'])) ? $parse_url['path'] : '')
-            .((isset($parse_url['query'])) ? '?' . $parse_url['query'] : '')
-            .((isset($parse_url['fragment'])) ? '#' . $parse_url['fragment'] : '')
-            ;
-    }
-}
+use Sb\UrlUtils as SbUrlUtils;
 
 class Console
 {
-
     private $cookiesJar = null;
     private $userAgent = null;
 
@@ -171,15 +67,14 @@ class Console
     {
         $urlParts = parse_url($url);
         if (!empty($urlParts['query'])) {
-            $urlParts['query'] .= '&' . http_build_query($params);
+            $urlParts['query'] .= '&' . SbUrlUtils::httpBuildUrl($params);
         }
-        return http_build_url($url, $urlParts);
+        return SbUrlUtils::httpBuildUrl($url, $urlParts);
     }
 
     public function request($url, $method = self::REQUEST_METHOD_GET, $params = array(), $redirect = 0, $isSubRequest = false)
     {
-
-        $paramsString = http_build_query($params);
+        $paramsString = SbUrlUtils::httpBuildUrl($params);
         if ($method == self::REQUEST_METHOD_GET) {
             $requestUrl = $this->mergeParams($url, $params);
             $ch = curl_init($requestUrl);
@@ -442,7 +337,7 @@ class Console
 
         $formInputData = array();
 
-        $inputs = $dom->find('input');
+        $inputs = $dom->find('input'); // input[type=text]
         /** @var \simple_html_dom_node[] $inputs  */
         foreach ($inputs as $input) {
             $inputType = $input->getAttribute('type');
@@ -466,58 +361,6 @@ class Console
         $result['fields'] = $formInputData;
 
         return $result;
-
-        /*
-        $action = $form->getAttribute('action');
-
-		if ($action) {
-			$action = $this->getAbsoluteUrl($action);
-		} else {
-			$action = $this->getClient()->getUri()->__toString();
-
-		}
-		// to-do
-		// относительный адрес определяется неправильно
-
-		$formData = array ('action' => $action,
-						   'method' => $form->getAttribute('method'),
-                           'enctype' => $form->getAttribute('enctype') ? $form->getAttribute('enctype') : Zend_Http_Client::ENC_URLENCODED);
-
-		$formInputData = array();
-
-		$inputs = $form->getElementsByTagName ( 'input' );
-		foreach ( $inputs as $input ) {
-			$inputName = $input->getAttribute ( 'name' );
-			if (! empty ( $inputName )) {
-				$formInputData [$inputName] = $input->getAttribute ( 'value' );
-			}
-		}
-
-		$selects = $form->getElementsByTagName ( 'select' );
-		foreach ( $selects as $select ) {
-
-			$options = $select->getElementsByTagName ( 'option' );
-			$optionsData = array ();
-			foreach ( $options as $option ) {
-				$optionsData [] = $option->getAttribute ( 'value' );
-			}
-
-			$formInputData [$select->getAttribute ( 'name' )] = $optionsData;
-		}
-
-		if ($withImages) {
-			$imgs = $form->getElementsByTagName ( 'img' );
-			foreach ( $imgs as $img ) {
-				$imageSrc = $img->getAttribute ( 'src' );
-				$image = array('src' => $imageSrc);
-				$image['data'] = base64_encode($this->doRequest($imageSrc, 'GET', true));
-				$formData['_images'][] = $image;
-			}
-		}
-
-		$formData ['fields'] = $formInputData;
-		return $formData;
-         */
     }
 
     public function getForms()
---
+++ src/Browser/Exception/Exception.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Sb\Browser\Exception;
+
+class Exception extends \Exception
+{
+
+}
